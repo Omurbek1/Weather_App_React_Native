08d5321e1635d35227a78d6bb0bad97e616e98d6
unknown <omurbekmamytbekov545@gmail.com>
2022-01-04 19:17:45 +0600

    weather app react_native

---
+++ App.js
@@ -0,0 +1,75 @@
+
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './component/Weather';
+import SearchBar from './component/SearchBar';
+
+const API_KEY = "77509e69515c87c5570c15b357de7f56";
+
+
+export default function App() {
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function fetchWeatherData(cityName) {
+        setLoaded(false);
+        const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=${API_KEY}`
+        try {
+            const response = await fetch(API);
+            if(response.status == 200) {
+                const data = await response.json();
+                setWeatherData(data);
+            } else {
+                setWeatherData(null);
+            }
+            setLoaded(true);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData('Бишкек');
+    }, [])
+    
+
+    if(!loaded) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator color='gray'  size={36} />
+            </View>
+
+        )
+    }
+
+    else if(weatherData === null) {
+        return (
+            <View style={styles.container}>
+                <SearchBar fetchWeatherData={fetchWeatherData}/>
+                <Text style={styles.primaryText}>Город не найден! Попробуйте другой город</Text>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}  />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
+});
